2d297b6b3d9d28e9255aa2f028ca28230b112556
isajal07 <isajal07@iSajal07-2.local>
2020-04-03 17:16:58 +0545

    Added signTransaction() and isValid() methods

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,10 +1,37 @@
 const SHA256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
 class Transaction {
   constructor(fromAddress, toAddress, amount) {
     this.fromAddress = fromAddress;
     this.toAddress = toAddress;
     this.amount = amount;
+  }
+
+  calculateHash() {
+    return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+  }
+
+  signTransaction(signingKey) {
+    if(signingKey.getPublic('hex') !== this.fromAddress) {
+      throw new Error('You cannot sign transactions for other wallets!');
+    }
+
+    const hashTx = this.calculateHash();
+    const sig = signingKey.sign(hashTx, 'base64');
+    this.signature = sig.toDER('hex');
+  }
+
+  isValid() {
+    if(this.fromAddress === null) return true;
+
+    if(!this.signature || this.signarure.lenght === 0) {
+      throw new Error('No signarure in this transaction');
+    }
+
+    const publiKey = ec.keyFromPublic(this.fromAddress, 'hex');
+    return publicKey.verify(this.calculateHash(), this.signature);
   }
 }
 
